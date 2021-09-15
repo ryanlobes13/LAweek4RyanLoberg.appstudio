58a6619c654a6d16f9f2f67a1ce1b97745f9fa1b
ryanlobes13 <90591769+ryanlobes13@users.noreply.github.com>
2021-09-15 16:29:47 -0500

    Initial commit

---
+++ forms/Pets/Pets.js
@@ -0,0 +1,13 @@
+let petNames = ['Darcy', 'Jack', 'Jerry', 'Marty', 'Vito', 'Vinny', 'Riley', 'Saddie', 'Marge', 'Sam']
+
+withSal = petNames
+withSal.push('Sal')
+console.log(withSal)
+
+
+console.log(`The third pet is named ${withSal[2]}`)
+let lengthwithSal = withSal.length
+
+console.log(lengthwithSal)
+ withSal.pop()
+ console.log(withSal)
